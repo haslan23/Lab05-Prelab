299205429ec77733478e62dfb8f829193e4f17d7
haslan23 <haslan23@ku.edu.tr>
2025-11-20 20:48:07 +0300

    Update q2.py

--- q2.py
+++ q2.py
@@ -2,4 +2,18 @@ def remove_adjacent_duplicates(s):
     '''
     Given a string remove all the adjacent duplicate characters and return the string
     '''
-    pass
+    ns=""
+        i=0
+        chn=False
+        while i<len(s):
+            if i+1<len(s) and s[i]==s[i+1]:
+                ch=s[i]
+                chn=True
+                while i<len(s) and s[i]==ch:
+                    i+=1
+            else:
+                ns+=s[i]
+                i+=1
+        if not chn:
+            return ns
+        s=ns
